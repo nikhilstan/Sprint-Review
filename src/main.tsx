384d8d35cfc4414899d33bc4a50bcf567a6448ea
Nikhil Pathak <nikhil.p@getstan.app>
2026-02-20 18:56:41 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
-import Lenis from "@studio-freight/lenis"
-
-const lenis = new Lenis({
-  smooth: true,
-  lerp: 0.08,
-})
-
-function raf(time: number) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
